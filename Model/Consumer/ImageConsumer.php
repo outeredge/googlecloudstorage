488637309168860cc77db7e78135752c10688beb
Bruno Dias <ibroopi@gmail.com>
2024-09-18 11:25:30 +0100

    Fix data type of communication

--- Model/Consumer/ImageConsumer.php
+++ Model/Consumer/ImageConsumer.php
@@ -14,14 +14,14 @@ class ImageConsumer
     public function processMessage($data): void
     {
     	/* Attempt to load the image from fallback URL and upload to GCS */
-        $ch = curl_init($data['url']);
+        $ch = curl_init($data[0]);
         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $content = curl_exec($ch);
 
         if ($content && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200) {
             $object = $this->storageObjectManagement->uploadObject($content, [
-                'name' => $data['prefixedPath'],
+                'name' => $data[1],
                 'predefinedAcl' => $this->storageObjectManagement->getObjectAclPolicy()
             ]);
         }
